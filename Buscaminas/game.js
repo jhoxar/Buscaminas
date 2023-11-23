a943067affadb481742a445f894fe3c1b91d9784
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2023-11-23 18:07:24 -0500

    Refactor code for to fill out game map with array methods

--- Buscaminas/game.js
+++ Buscaminas/game.js
@@ -1,63 +1,64 @@
 console.log(maps)
 const d = document;
-window.addEventListener('load',startGame )
-
-/*Se accede al elemento canvas*/ 
+window.addEventListener('load', setCanvasSize )
+window.addEventListener('resize', setCanvasSize)
 const canvas = d.querySelector('#canvas') 
-/*se accede al contexto canvas para poder acceder a los metodos que tiene canvas. */
 const game = canvas.getContext('2d') 
+let canvasSize;
+let elementSize;
 
 function startGame(){
 
-    let canvasSize;
+    elementSize = (canvasSize / 10)  - 1
+
+    game.font = elementSize + 'px Verdana'
+    game.textAlign = "start"
+
+    const map = maps[0]
+    const mapRows = map.trim().split('\n')
+    const mapRowCols = mapRows.map(row => row.trim().split(''))
+
+    console.log({
+    mapRows,
+    mapRowCols
+    })
+
+   mapRowCols.forEach((row, rowI)=>{
+    row.forEach((col, colI)=>{
+        const emoji = emojis[col]
+        const posX = elementSize *(colI)
+        const posY = elementSize *(rowI + 1)
+        game.fillText(emoji, posX , posY)
+    
+        console.log({row, rowI})
+        console.log({col, colI})
+    })
+   })
+
+
+    /* for(let row = 0 ; row <=10; row++){
+        for(let col = 0 ; col <10; col++)
+        game.fillText(emojis[mapRowCols[row][col]], elementSize * col   , elementSize * row + 50 )
+
+    } */
+    
+    console.log({canvasSize, elementSize})
+    console.log(window.innerHeight)
+    console.log(window.innerWidth)
+}
+
+function setCanvasSize(){
 
     if(window.innerWidth > window.innerHeight){
-        canvasSize = window.innerHeight * 0.8
+        canvasSize = window.innerHeight * 0.7
     }else{
-        canvasSize = window.innerWidth * 0.8
+        canvasSize = window.innerWidth * 0.7
     }
 
     canvas.setAttribute('width', canvasSize)
     canvas.setAttribute('height', canvasSize)
 
-    const elementSize = (canvasSize / 10)  - 1
-
-    game.font = elementSize + 'px Verdana'
-    game.textAlign = ""
-
-
-    
-   /*  for(let i=0; i<10; i++){
-        for(let z=1 ; z<11; z++)
-        
-
-    } */
-
-    for(let i = 0 ; i<10; i++){
-        for(let z = 1 ; z<=10; z++)
-        game.fillText(emojis['X'], elementSize * i  , elementSize * z )
-
-    }
-    
-
-    console.log({canvasSize, elementSize})
-
-
-    
-    console.log(window.innerHeight)
-    console.log(window.innerWidth)
-   
-
-    //game.fillRect(0,0,100,100)
-    //game.clearRect(50,0,50,50)
-    //game.clearRect(0,50,50,50)
-    //game.clearRect(0,0,50,100)
-    //game.clearRect(50,0,50,100)
-
-    /* game.fillStyle = 'red'
-    game.font= '30px verdana'
-    game.textAlign = 'center'
-    game.fillText('Dreamer', 50 , 50) */
+    startGame()
 
 }
 
